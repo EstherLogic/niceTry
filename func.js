6f1300523b325450c52b512fc4a0bca2d12f3ce0
EstherLogic <ep0527108676@gmail.com>
2025-11-19 22:27:25 +0200

    Moved click event to JS file

--- func.js
+++ func.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('.but');
 
-button.addEventListener('click', function() {
-    alert('Have a good day!😍');
+button.addEventListener('click', function () {
+    let answer = confirm("Are you ready?");
+    console.log(answer); 
+
 });
