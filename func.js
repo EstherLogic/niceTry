4d9a2d91a438783879443a9d1e254d61eb34ee78
EstherLogic <ep0527108676@gmail.com>
2025-11-19 22:39:00 +0200

    change func.js to enter name

--- func.js
+++ func.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('.but');
 
-button.addEventListener('click', function() {
-    alert('Have a good day!😍');
+button.addEventListener('click', function () {
+    let name = prompt("Enter your name:");
+    console.log(name);
+
 });
