f6977f073fa550358a3acb43002abd6122b7e532
EstherLogic <ep0527108676@gmail.com>
2025-11-24 01:01:08 +0200

    version#1

--- func.js
+++ func.js
@@ -1,9 +1,5 @@
 const button = document.querySelector('.but');
 
 button.addEventListener('click', function () {
-    let name = prompt("Enter your name:");
-    console.log(name);
-    let answer = confirm("Are you ready?");
-    console.log(answer); 
-
+alert('Button clicked!');
 });
